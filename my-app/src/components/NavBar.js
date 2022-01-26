6545035bc79c05fe8de39ba2f8025ca2e544ef2c
alex campe <campesatoalexis@gmail.com>
2022-01-26 11:33:36 -0300

    segundaclase

---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css'
+
+export default function NavBar() {
+return (<div>
+    <a href="imagenes/logopino.jpg"></a>
+    <ul>
+        
+        <li>PinoBlanco</li>
+        <li>Nosotros</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+    </ul>
+    </div>
+);
+}
