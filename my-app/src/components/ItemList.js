267b1a5d73ade9f286d616f0f25faba8fdc67a85
alex campe <campesatoalexis@gmail.com>
2022-02-09 00:11:16 -0300

    promises2

--- my-app/src/components/ItemList.js
+++ my-app/src/components/ItemList.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import './ItemList.css';
 
 
 export default function ItemList({products}){
@@ -8,23 +7,20 @@ export default function ItemList({products}){
 
     return (
         <div>
+
            {
-               
-
-
-               products.map(function(producto) {
-                return (
-                 <div key={producto.id}>
-                     <img src={producto.pictureUrl} alt='Imagen del producto'/>
-                     <p className="price">{producto.price}</p>
-                     <p className="title">{producto.title}</p>
-                 <div/>
-                )
-            })
-
-
-
-           }
+                products.map(function(producto){
+                    return(
+                        <div key={producto.id}>
+                            <img src={producto.pictureUrl} alt="Imagen del producto"/>
+                            <p className="price">{producto.price}</p>
+                            <p className="title">{producto.title}</p>
+                        </div>
+                    )
+                })
+            
+                
+            }
         </div>
-    )
+    );
 }
