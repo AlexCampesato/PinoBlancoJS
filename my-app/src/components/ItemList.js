9b3c95c817434501d3a285a5543c8a9d60e77f24
alex campe <campesatoalexis@gmail.com>
2022-02-15 11:16:24 -0300

    navegacion

--- my-app/src/components/ItemList.js
+++ my-app/src/components/ItemList.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import './ItemList.css'
 
+import './ItemList.css'
+import {Link} from 'react-router-dom';
 
 
 export default function ItemList({products}){
@@ -14,9 +14,11 @@ export default function ItemList({products}){
                 products.map(function(producto){
                     return(
                         <div className="item-container" key={producto.id}>
+                            
                             <img className="item-picture" src={producto.pictureUrl} alt="Imagen del producto"/>
                             <p className="item-title">{producto.title}</p>
-                            <p className="item-price">{producto.price}</p>
+                            <Link to={'/producto/${producto.id}'}/>
+                            <p className="item-price">USD {producto.price}</p>
                             
                         </div>
                     )
