7e90bc8fefe156c078f2e6b1cb3c5fb5b8f67935
alex campe <campesatoalexis@gmail.com>
2022-02-23 09:21:15 -0300

    arreglo context

--- my-app/src/components/Cart.js
+++ my-app/src/components/Cart.js
@@ -4,13 +4,15 @@ import { CartContext } from "../context/CartContext";
 const Cart=() =>{
 
     const {cart, vaciarCarrito}=useContext(CartContext);
+    console.log(cart)
 
     return (
     <>
         {cart.map((producto) => (
             <div key={producto.id}>
-                <h3>{producto.name}</h3>
-                <h1>hola</h1>
+                <h2>{producto.title}</h2>
+                <h3>Usted llevara: {producto.cantidad} unidades</h3>
+                
             </div>
         ))}
         <button onClick={vaciarCarrito}>Vaciar Carrito</button>
