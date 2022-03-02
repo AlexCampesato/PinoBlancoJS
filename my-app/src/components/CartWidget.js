9b1fa36be22bab493ae9a7147657f706098307e5
alex campe <campesatoalexis@gmail.com>
2022-03-02 01:01:32 -0300

    cartWidget

--- my-app/src/components/CartWidget.js
+++ my-app/src/components/CartWidget.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+
+import React, {useContext, useEffect, useState} from "react";
+import { CartContext } from "../context/CartContext";
 import './CartWidget.css';
 import CarritoImagen from './carrito.svg'
 
-export default function Cartwidget() {
-    
-    return (
-        <img src={CarritoImagen} alt="Carrito" class='Carrito' />
-       
-    );
-    }
-    
+const Cartwidget=() =>{
+
+    const {cart}=useContext(CartContext);
+    const [isCartEmpty, setIsCartEmpty] = useState(true)
+
+
+    useEffect(() => {
+        if (cart.length > 0) {
+        setIsCartEmpty(false)
+        }
+    }, [cart]);
+
+    if (isCartEmpty) {
+        return (<img src=""/>
+              )
+    } else {
+        return (<img src={CarritoImagen} alt="Carrito" class='Carrito' />)
+}};
+
+export default Cartwidget;
